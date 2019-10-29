735a7ceab5ca45fd6e9bd1e30d4642fde5097f30
Miguel C <mkarmona@gmail.com>
2019-10-29 18:41:28 +0000

    wip: implement harmonic sum with a rudimentary dsl for hackable SQL now fully compile and translate

--- app/elesecu/Column.scala
+++ app/elesecu/Column.scala
@@ -1,16 +1,19 @@
 package elesecu
 
 case class Column(raw: Expression, alias: Option[String]) {
-  override def toString: String = {
+  lazy val rep: String = {
     val ex = raw match {
       case _: EmptyExpression.type => None
       case _ => Some(raw.toString)
     }
     List(ex, alias).withFilter(_.isDefined).map(_.get).mkString("", " AS ", "")
   }
+
+  override def toString: String = rep
+
   def as(newAlias: Option[String]): Column = Column(raw, newAlias)
   def name: Column = alias.map(Column.apply).getOrElse(expr)
-  def expr: Column = Column(raw)
+  def expr: Column = Column(raw, None)
 }
 
 object Column {
@@ -29,7 +32,7 @@ object Column {
   def apply(name: String): Column = parse(name)
   def apply(ex: String, asName: String): Column = parse(ex).as(Some(asName))
 
-  def expr(name: String): Column = Column(name)
+  def column(name: String): Column = Column(name)
 
   def lit[T](v: T): Column =  {
     v match {
@@ -38,23 +41,23 @@ object Column {
     }
   }
 
-  def uniq(cols: Seq[Column]): Column = expr(cols.map(_.name).mkString("uniq(", ",", ")"))
+  def uniq(cols: Seq[Column]): Column = column(cols.map(_.name).mkString("uniq(", ",", ")"))
   def uniq(col: Column, cols: Column*): Column = uniq(col +: cols)
-  def groupArray(col: Column): Column = expr(s"groupArray(${col.name})")
-  def arrayMap(lambda: String, col: Column): Column = expr(s"arrayMap($lambda,${col.name})")
-  def any(col: Column): Column = expr(s"any(${col.name})")
-  def array(cols: Seq[Column]): Column = expr(cols.map(_.name).mkString("array(",",", ")"))
+  def groupArray(col: Column): Column = column(s"groupArray(${col.name})")
+  def arrayMap(lambda: String, col: Column): Column = column(s"arrayMap($lambda,${col.name})")
+  def any(col: Column): Column = column(s"any(${col.name})")
+  def array(cols: Seq[Column]): Column = column(cols.map(_.name).mkString("array(",",", ")"))
   def array(col: Column, cols: Column*): Column = array(col +: cols)
   def joinGet(tableName: String, fieldName: String, col: Column): Column =
-    expr(s"joinGet('$tableName','$fieldName',${col.name})")
-  def flatten(col: Column): Column = expr(s"flatten(${col.name})")
-  def arraySort(col: Column): Column = expr(s"arraySort(${col.name})")
-  def arrayReverseSort(col: Column): Column = expr(s"arrayReverseSort(${col.name})")
+    column(s"joinGet('$tableName','$fieldName',${col.name})")
+  def flatten(col: Column): Column = column(s"flatten(${col.name})")
+  def arraySort(col: Column): Column = column(s"arraySort(${col.name})")
+  def arrayReverseSort(col: Column): Column = column(s"arrayReverseSort(${col.name})")
   def arraySlice(col: Column, pos: Int, size: Int): Column =
-    expr(s"arraySlice(${col.name},${lit(pos).name},${lit(size).name})")
-  def length(col: Column): Column = expr(s"length(${col.name})")
-  def range(col: Column): Column = expr(s"range(${col.name})")
+    column(s"arraySlice(${col.name},${lit(pos).name},${lit(size).name})")
+  def length(col: Column): Column = column(s"length(${col.name})")
+  def range(col: Column): Column = column(s"range(${col.name})")
   def arraySum(lambda: String, col1: Column, col2: Column): Column =
-    expr(s"arraySum($lambda,${col1.name},${col2.name})")
-  def div(col1: Column, col2: Column): Column = expr(s"(${col1.name} / ${col2.name})")
+    column(s"arraySum($lambda,${col1.name},${col2.name})")
+  def div(col1: Column, col2: Column): Column = column(s"(${col1.name} / ${col2.name})")
 }
--- app/models/entities/Harmonic.scala
+++ app/models/entities/Harmonic.scala
@@ -16,31 +16,15 @@ object Harmonic {
   def maxHSColumn: Column =
     lit(Harmonic.maxValue(maxVectorElementsDefault, pExponentDefault, 1.0)).as(Some("max_hs"))
 
-  def mkHSColumn(col: Column, maxHS: Column): Seq[Column] = {
-    //    1.6349839001848923 as max_hs,
-    //    2.6731723538638033 as max_hs_total,
-    //    arrayReverseSort(flatten(groupArray(datasource_europepmc.scores))) as europepmc,
-    //    arrayReverseSort(flatten(groupArray(datasource_cancer_gene_census.scores))) as cancer_gene_census,
-    //    arrayReverseSort(flatten(groupArrayIf(datasource_chembl.scores, target_id = 'ENSG00000091831'))) as chembl,
-    //    arrayReverseSort(flatten(groupArray(datasource_crispr.scores))) as crispr,
-    //    arrayReverseSort(flatten(groupArray(datasource_eva.scores))) as eva,
-    //    arrayReverseSort(flatten(groupArray(datasource_eva_somatic.scores))) as eva_somatic,
-    //    arrayReverseSort(flatten(groupArray(datasource_expression_atlas.scores))) as expression_atlas,
-    //
-    //     arraySlice(europepmc, 1, 100) as ds_epmc_v,
-    //     arraySum((a, b) -> a / pow((b + 1),2), ds_epmc_v, range(length(ds_epmc_v))) / max_hs as ds_europepmc_hs,
+  def mkHSColumn(col: Column, maxHS: Column): IndexedSeq[Column] = {
+    val colName = Some(col.name.toString + "_v")
 
-    val colName = col.alias match {
-      case Some(n) => Some(n + "_v")
-      case None => Some(UUID.randomUUID().toString)
-    }
-    val dsV = arraySlice(arrayReverseSort(flatten(groupArray(col))),1, maxVectorElementsDefault)
+    val dsV = arraySlice(arrayReverseSort(flatten(groupArray(col.name))),1, maxVectorElementsDefault)
       .as(colName)
 
-    val dsVHS = div(arraySum("(a, b) -> a / pow((b + 1),2)", dsV, range(length(dsV))), maxHS)
+    val dsVHS = div(arraySum("(a, b) -> a / pow((b + 1),2)", dsV.name, range(length(dsV.name))), maxHS)
         .as(colName.map(_ + "_hs"))
 
-    List(dsV, dsVHS)
-
+    Vector(dsV, dsVHS)
   }
 }
