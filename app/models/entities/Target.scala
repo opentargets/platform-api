fff53ff317feaa4942097b5caad829c89f109ae9
Miguel C <mkarmona@gmail.com>
2020-03-19 17:40:46 +0000

    wip: include Orthologs, safety, GeneOntology and meny others
    still json formatters

--- app/models/entities/Target.scala
+++ app/models/entities/Target.scala
@@ -28,8 +28,61 @@ case class Orthologs(chimpanzee: Option[Seq[Ortholog]],
                      zebrafish: Option[Seq[Ortholog]]
                     )
 
+case class Reference(pubmedId: String, description: String)
+
+case class Hallmark(suppress: Boolean,
+                    promote: Boolean,
+                    reference: Reference,
+                    label: String)
+
+case class HallmarkAttribute(name: String, reference: Reference)
+
+case class CancerHallmarks(hallmarks: Seq[Hallmark], attributes: Seq[HallmarkAttribute], functions: Seq[Reference])
+
 case class Protein(id: String, accessions: Seq[String], functions: Seq[String])
+
 case class GenomicLocation(chromosome: String, start: Long, end: Long, strand: Int)
+
+case class SourceLink(source: String, link: String)
+
+case class PortalProbe(note: String, chemicalprobe: String, geneSymbol: String, sourcelinks: Seq[SourceLink])
+
+case class ChemicalProbes(probminer: String, portalprobes: Seq[PortalProbe])
+
+case class GeneOntology(id: String, project: String, term: String, evidenceId: String)
+
+case class TractabilityAntibodyCategories(predictedTractableMedLowConfidence: Double,
+                                          clinicalPrecedence: Double,
+                                          predictedTractableHighConfidence: Double)
+case class TractabilitySmallMoleculeCategories(clinicalPrecedence: Double,
+                                               predictedTractable: Double,
+                                               discoveryPrecedence: Double)
+case class TractabilityAntibody(topCategory: String, buckets: Seq[Long], categories: TractabilityAntibodyCategories)
+case class TractabilitySmallMolecule(topCategory: String, smallMoleculeGenomeMember: Boolean,
+                                     buckets: Seq[Long],
+                                     highQualityCompounds: Long,
+                                     ensemble: Double,
+                                     categories: TractabilitySmallMoleculeCategories
+                                    )
+case class Tractability(smallmolecule: Option[TractabilitySmallMolecule], antibody: Option[TractabilityAntibody])
+
+case class SafetyCode(code: String, mappedTerm: String, termInPaper: String)
+case class SafetyReference(pubmedId: Option[String], refLabel: Option[String], refLink: Option[String])
+case class AdverseEffectsActivationEffects(acuteDosing: Seq[SafetyCode], chronicDosing: Seq[SafetyCode],
+                                           general: Seq[SafetyCode])
+case class AdverseEffectsInhibitionEffects(acuteDosing: Seq[SafetyCode],
+                                           chronicDosing: Seq[SafetyCode], general: Seq[SafetyCode],
+                                           developmental: Seq[SafetyCode])
+
+case class AdverseEffects(activationEffects: AdverseEffectsActivationEffects,
+                          inhibitionEffects: AdverseEffectsInhibitionEffects,
+                          organsSystemsAffected: Seq[SafetyCode],
+                          references: Seq[SafetyReference])
+
+case class SafetyRiskInfo(organsSystemsAffected: Seq[SafetyCode], references: Seq[SafetyReference],
+                          safetyLiability: String)
+case class Safety(adverseEffects: AdverseEffects, safetyRiskInfo: SafetyRiskInfo)
+
 case class Target(id: String,
                   approvedSymbol: String,
                   approvedName: String,
@@ -39,7 +92,11 @@ case class Target(id: String,
                   symbolSynonyms: Seq[String],
                   genomicLocation: GenomicLocation,
                   proteinAnnotations: Option[Protein],
-                  orthologs: Option[Orthologs]
+                  orthologs: Option[Orthologs],
+                  cancerHallmarks: Option[CancerHallmarks],
+                  chemicalProbes: Option[ChemicalProbes],
+                  geneOntology: Seq[GeneOntology],
+                  safety: Option[Safety]
                  )
 
 object Target {
