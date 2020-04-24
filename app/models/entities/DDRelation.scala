87d4951e737d2988a47144ed098dc193c2224598
Miguel C <mkarmona@gmail.com>
2020-04-24 10:19:15 +0100

    fix: typo relations AndB by AAndB

--- app/models/entities/DDRelation.scala
+++ app/models/entities/DDRelation.scala
@@ -7,7 +7,7 @@ import play.api.libs.functional.syntax._
 
 case class DDRelation(id: String, A: String, B: String,
                       countA: Long, countB: Long,
-                      countAOrB: Long, countAndB: Long,
+                      countAOrB: Long, countAAndB: Long,
                       score: Double)
 
 case class DDRelations(maxCountAOrB: Long, count: Long, rows: Seq[DDRelation])
