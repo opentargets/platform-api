b43ba18d2e5938f0badaf9e6dfed00dfda70cf01
Miguel C <mkarmona@gmail.com>
2020-12-18 00:50:56 +0000

    fix: include proper references and also clinical trial ids too although it breaks search after because the phase is now integer and not string

--- app/models/entities/KnownDrug.scala
+++ app/models/entities/KnownDrug.scala
@@ -6,11 +6,13 @@ import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
 
 case class URL(url: String, name: String)
+case class KnownDrugReference(source: String, ids: Seq[String], urls: Seq[String])
 
 case class KnownDrug(approvedSymbol: String, approvedName: String, label: String, prefName: String,
                      drugType: String, targetId: String, diseaseId: String,
-                     drugId: String, phase: String, mechanismOfAction: String,
+                     drugId: String, phase: Int, mechanismOfAction: String,
                      status: Option[String], targetClass: Seq[String],
+                     references: Seq[KnownDrugReference],
                      ctIds: Seq[String], urls: Seq[URL])
 
 case class KnownDrugs(uniqueDrugs: Long,
@@ -23,6 +25,7 @@ case class KnownDrugs(uniqueDrugs: Long,
 object KnownDrug extends Logging {
   val ctPattern = "NCT(\\d{8})".r
 
+  implicit val KnownDrugReferenceImpJSONF = Json.format[KnownDrugReference]
   implicit val URLImpW = Json.writes[URL]
   implicit val URLImpR: Reads[URL] = (
     (__ \ "url").read[String] and
@@ -40,11 +43,12 @@ object KnownDrug extends Logging {
       (__ \ "targetId").read[String] and
       (__ \ "diseaseId").read[String] and
       (__ \ "drugId").read[String] and
-      (__ \ "phase").read[String] and
+      (__ \ "phase").read[Int] and
       (__ \ "mechanismOfAction").read[String] and
       (__ \ "status").readNullable[String] and
       (__ \ "targetClass").readWithDefault[Seq[String]](Seq.empty) and
-      (__ \ "references").readWithDefault[Seq[Map[String, String]]](Seq.empty).map(
+      (__ \ "references").readWithDefault[Seq[KnownDrugReference]](Seq.empty) and
+      (__ \ "urls").readWithDefault[Seq[Map[String, String]]](Seq.empty).map(
         s => s.flatMap(m => ctPattern findAllIn m("url"))
       ) and
       (__ \ "urls").read[Seq[URL]]
