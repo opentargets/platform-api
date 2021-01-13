5127c50ef79191475ef582931200568d27bfd27e
Jarrod Baker <jarrod.bakeriii@gmail.com>
2021-01-13 10:54:53 +0000

    Update formatting on test/*

--- test/controllers/GraphQLControllerTest.scala
+++ test/controllers/GraphQLControllerTest.scala
@@ -16,20 +16,27 @@ import play.api.test.Helpers.{POST, contentAsString}
 import play.api.test.{FakeRequest, Injecting}
 import test_configuration.IntegrationTestTag
 
-class GraphQLControllerTest extends PlaySpec with GuiceOneAppPerTest with Injecting with DrugInputs with AdverseEventInputs
-  with Logging with TableDrivenPropertyChecks {
+class GraphQLControllerTest
+    extends PlaySpec
+    with GuiceOneAppPerTest
+    with Injecting
+    with DrugInputs
+    with AdverseEventInputs
+    with Logging
+    with TableDrivenPropertyChecks {
 
   implicit lazy val timeout: Timeout = Timeout(1120, TimeUnit.SECONDS)
   lazy val controller: GraphQLController = inject[GraphQLController]
 
   "Drug queries" must {
     "return an id " taggedAs IntegrationTestTag in {
-      val responses: Seq[String] = chemblIds.map(idQuery).map { q => {
-        val request = FakeRequest(POST, "/graphql")
-          .withHeaders(("Content-Type", "application/json"))
-          .withBody(q)
-        contentAsString(controller.gqlBody.apply(request))
-      }
+      val responses: Seq[String] = chemblIds.map(idQuery).map { q =>
+        {
+          val request = FakeRequest(POST, "/graphql")
+            .withHeaders(("Content-Type", "application/json"))
+            .withBody(q)
+          contentAsString(controller.gqlBody.apply(request))
+        }
       }
       val results = responses.zip(chemblIds).filter(s => s._1.contains("errors"))
       logger.info(s"${responses.size - results.size} entries had no errors.")
@@ -44,14 +51,15 @@ class GraphQLControllerTest extends PlaySpec with GuiceOneAppPerTest with Inject
     "return full objects" taggedAs IntegrationTestTag in {
       val tests = chemblIds.length
       var i = 1
-      val responses: Seq[String] = chemblIds.map(fullQuery).map { q => {
-        val request = FakeRequest(POST, "/graphql")
-          .withHeaders(("Content-Type", "application/json"))
-          .withBody(q)
-        logger.info(s"Test $i of $tests")
-        i = i + 1
-        contentAsString(controller.gqlBody.apply(request))
-      }
+      val responses: Seq[String] = chemblIds.map(fullQuery).map { q =>
+        {
+          val request = FakeRequest(POST, "/graphql")
+            .withHeaders(("Content-Type", "application/json"))
+            .withBody(q)
+          logger.info(s"Test $i of $tests")
+          i = i + 1
+          contentAsString(controller.gqlBody.apply(request))
+        }
       }
       val results = responses.zip(chemblIds).filter(s => s._1.contains("errors"))
       logger.info(s"${responses.size - results.size} entries had no errors.")
@@ -80,11 +88,15 @@ class GraphQLControllerTest extends PlaySpec with GuiceOneAppPerTest with Inject
           .withHeaders(("Content-Type", "application/json"))
           .withBody(parentChildMoAQuery(p, c))
         val jsObject = Json.parse(contentAsString(controller.gqlBody.apply(request)))
-        val drugs: Seq[ParentChildMoAReturn] = (jsObject \ "data" \ "drugs").as[JsArray].value.map(_.as[ParentChildMoAReturn])
+        val drugs: Seq[ParentChildMoAReturn] =
+          (jsObject \ "data" \ "drugs").as[JsArray].value.map(_.as[ParentChildMoAReturn])
         val (parent, child) = (drugs.head, drugs.tail.head)
-        assert(parent.mechanismsOfAction.get.uniqueTargetTypes sameElements child.mechanismsOfAction.get.uniqueTargetTypes)
-        assert(parent.mechanismsOfAction.get.uniqueActionTypes sameElements child.mechanismsOfAction.get.uniqueActionTypes)
-        assert(parent.mechanismsOfAction.get.rows.length == child.mechanismsOfAction.get.rows.length)
+        assert(
+          parent.mechanismsOfAction.get.uniqueTargetTypes sameElements child.mechanismsOfAction.get.uniqueTargetTypes)
+        assert(
+          parent.mechanismsOfAction.get.uniqueActionTypes sameElements child.mechanismsOfAction.get.uniqueActionTypes)
+        assert(
+          parent.mechanismsOfAction.get.rows.length == child.mechanismsOfAction.get.rows.length)
       }
 
     }
@@ -94,13 +106,12 @@ class GraphQLControllerTest extends PlaySpec with GuiceOneAppPerTest with Inject
       forAll(testCases) { (p: String, c: String) =>
         val request: FakeRequest[JsValue] = FakeRequest(POST, "/graphql")
           .withHeaders(("Content-Type", "application/json"))
-          .withBody(parentChildLinkedTargetQuery(p,c))
+          .withBody(parentChildLinkedTargetQuery(p, c))
         val jsObject = Json.parse(contentAsString(controller.gqlBody.apply(request)))
-        val drugs = (jsObject \ "data" \ "drugs").as[JsArray].value.map(_.as[ParentChildLinkedTargets])
-        val (d1,d2) = (drugs.head, drugs.tail.head)
-        sForAll(drugs)(d =>
-          assert(d.linkedTargets.isDefined)
-        )
+        val drugs =
+          (jsObject \ "data" \ "drugs").as[JsArray].value.map(_.as[ParentChildLinkedTargets])
+        val (d1, d2) = (drugs.head, drugs.tail.head)
+        sForAll(drugs)(d => assert(d.linkedTargets.isDefined))
         d1.linkedTargets.get.count should equal(d2.linkedTargets.get.count)
       }
 
@@ -109,12 +120,13 @@ class GraphQLControllerTest extends PlaySpec with GuiceOneAppPerTest with Inject
 
   "Adverse event queries" must {
     "return valid objects when an adverse event is associated with the id" taggedAs IntegrationTestTag in {
-      val responses: Seq[String] = aeChemblIds.map(simpleAeQuery).map { q => {
-        val request = FakeRequest(POST, "/graphql")
-          .withHeaders(("Content-Type", "application/json"))
-          .withBody(q)
-        contentAsString(controller.gqlBody.apply(request))
-      }
+      val responses: Seq[String] = aeChemblIds.map(simpleAeQuery).map { q =>
+        {
+          val request = FakeRequest(POST, "/graphql")
+            .withHeaders(("Content-Type", "application/json"))
+            .withBody(q)
+          contentAsString(controller.gqlBody.apply(request))
+        }
       }
       val results = responses.zip(chemblIds).filter(s => s._1.contains("errors"))
       logger.info(s"${responses.size - results.size} entries had no errors.")
