ed8b3b5447e45233d52ef8eeb066281a80f145a0
Jarrod Baker <43906063+JarrodBaker@users.noreply.github.com>
2021-06-01 11:08:26 +0100

    Complete test coverage for GraphQL queries from FE (#64)
    * Add Drug generator and adverse event query.
    
    - Add drug warning query
    
    * Add transformer query parameter to customise GqlCase queries.
    
    - Add tests for bibliography
    - Add ClickhouseTestTag
    
    * Add many GraphQL test cases.
    
    * Add support for testing fragments.
    
    * Add fragments for target and disease and more test cases.
    
    * Add many more test cases for GQL
    
    * Adding tests for summary fragment on disease with gene argument.
    
    * Add association and known drugs query tests.
    
    * Add safety summary query.
    
    * Remove redundant queries

--- test/inputs/GqlCase.scala
+++ test/inputs/GqlCase.scala
@@ -1,11 +1,199 @@
 package inputs
 
+import controllers.GqlTest
+import models.entities.AggregationFilter
 import org.scalacheck.Gen
+import play.api.Logging
 
-sealed trait GqlCase[T] extends GqlItTestInputs {
+sealed trait GqlCase[T] extends GqlItTestInputs with Logging {
   val file: String
   val inputGenerator: Gen[T]
+
   def generateVariables(inputs: T): String
+}
+
+sealed trait GqlFragment[T] extends GqlCase[T] {
+  // first capture group is name of fragment ot insert into query
+  val fragmentRegex = """^fragment ([\w]+) on [\w]+""".r
+
+  lazy val fragmentQuery: String = GqlTest.getQueryFromFile(file)
+  lazy val fragmentName: String = fragmentRegex findFirstMatchIn fragmentQuery match {
+    case Some(value) => value.group(1)
+    case None =>
+      logger.error(s"Unable to extract fragment name from $file.")
+      ""
+  }
+
+  def generateFragmentQuery: String
+}
+
+case class AssociationDisease(file: String) extends GqlCase[(String, AggregationFilter)] {
+  val inputGenerator = for {
+    disease <- diseaseGenerator
+    agg <- aggregationfilterGenerator
+  } yield (disease, agg)
+
+  def generateVariables(inputs: (String, AggregationFilter)) = {
+    s"""
+      "variables": {
+      "efoId": "${inputs._1}",
+      "index": 0,
+      "size": 10,
+      "sortBy": "",
+      "aggregationFilters": [
+        {
+        "name": "${inputs._2.name}",
+        "path": ${inputs._2.path.mkString("[\"", "\", \"", "\"]")}
+        }]
+    }
+    """
+  }
+}
+
+case class AssociationTarget(file: String) extends GqlCase[(String, AggregationFilter)] {
+  val inputGenerator = for {
+    target <- geneGenerator
+    agg <- aggregationfilterGenerator
+  } yield (target, agg)
+
+  def generateVariables(inputs: (String, AggregationFilter)) = {
+    s"""
+      "variables": {
+      "ensemblId": "${inputs._1}",
+      "index": 0,
+      "size": 10,
+      "sortBy": "",
+      "aggregationFilters": [
+        {
+        "name": "${inputs._2.name}",
+        "path": ${inputs._2.path.mkString("[\"", "\", \"", "\"]")}
+        }]
+    }
+    """
+  }
+}
+
+case class Disease(file: String) extends GqlCase[String] {
+  val inputGenerator = diseaseGenerator
+
+  def generateVariables(disease: String): String =
+    s"""
+      "variables": {
+      "efoId": "$disease",
+      "size": 10,
+      "index": 0
+    }
+    """
+}
+
+case class DiseaseAggregationfilter(file: String) extends GqlCase[(String, AggregationFilter)] {
+  val inputGenerator = for {
+    disease <- diseaseGenerator
+    agg <- aggregationfilterGenerator
+  } yield (disease, agg)
+
+  def generateVariables(inputs: (String, AggregationFilter)): String =
+    s"""
+      "variables": {
+      "efoId": "${inputs._1}",
+      "aggregationFilters": [
+        {
+        "name": "${inputs._2.name}",
+        "path": ${inputs._2.path.mkString("[\"", "\", \"", "\"]")}
+        }]
+    }
+    """
+}
+
+abstract class AbstractDrug extends GqlCase[String] {
+  val inputGenerator = drugGenerator
+
+  override def generateVariables(drugId: String): String = {
+    s"""
+      "variables": {
+      "chemblId": "$drugId"
+    }
+    """
+  }
+}
+
+abstract class AbstractTarget extends GqlCase[String] {
+  val inputGenerator = geneGenerator
+
+  def generateVariables(target: String): String = {
+    s"""
+      "variables": {
+      "ensgId": "$target",
+      "size": 10,
+      "index": 0
+    }
+    """
+  }
+}
+
+abstract class AbstractDisease extends GqlCase[String] {
+  val inputGenerator = diseaseGenerator
+
+  def generateVariables(disease: String): String = {
+    s"""
+      "variables": {
+      "efoId": "$disease"
+    }
+    """
+  }
+}
+
+case class DrugFragment(file: String) extends AbstractDrug with GqlFragment[String] {
+
+  def generateFragmentQuery: String =
+    s"$fragmentQuery query DrugFragment(xyz: String!) { drug(chemblId: xyz) { ...$fragmentName } }"
+      .replace("xyz", "$chemblId")
+}
+
+case class TargetFragment(file: String) extends AbstractTarget with GqlFragment[String] {
+  def generateFragmentQuery: String =
+    s"$fragmentQuery query TargetFragment(xyz: String!) { target(ensemblId: xyz) { ...$fragmentName } }"
+      .replace("xyz", "$ensgId")
+}
+
+case class DiseaseFragment(file: String) extends AbstractDisease with GqlFragment[String] {
+  def generateFragmentQuery: String =
+    s"$fragmentQuery query DiseaseFragment(xyz: String!) { disease(efoId: xyz) { ...$fragmentName } }"
+      .replace("xyz", "$efoId")
+}
+
+/*
+This is a fragment on disease which takes a gene as an argument. Used on the FE to create
+summary information.
+ */
+case class DiseaseSummaryFragment(file: String) extends GqlFragment[(String, String)] {
+  def generateFragmentQuery: String = s"$fragmentQuery query DiseaseFragment(xyz: String!, ensgId: String) { disease(efoId: xyz) { ...$fragmentName } }"
+    .replace("xyz", "$efoId")
+
+  val inputGenerator = targetDiseaseGenerator
+
+  def generateVariables(inputs: (String, String)) =
+    s"""
+      "variables": {
+      "ensgId": "${inputs._1}",
+      "efoId": "${inputs._2}"
+    }
+    """
+}
+
+case class Drug(file: String) extends AbstractDrug with GqlCase[String]
+
+case class KnownDrugs(file: String) extends GqlCase[String] {
+  val inputGenerator = diseaseGenerator
+
+  def generateVariables(disease: String) =
+    s"""
+      "variables": {
+      "efoId": "$disease",
+      "cursor": null,
+      "freeTextQuery": null
+    }
+    """
 }
 
 case class Search(file: String) extends GqlCase[String] {
@@ -39,20 +227,7 @@ case class SearchPage(file: String) extends GqlCase[(String, String, Int)] {
     """
 }
 
-case class Target(file: String) extends GqlCase[String] {
-
-  val inputGenerator = geneGenerator
-
-  def generateVariables(target: String): String = {
-    s"""
-      "variables": {
-      "ensgId": "$target",
-      "size": 10,
-      "index": 0
-    }
-    """
-  }
-}
+case class Target(file: String) extends AbstractTarget with GqlCase[String]
 
 case class TargetDisease(file: String) extends GqlCase[(String, String)] {
   val inputGenerator = for {
@@ -64,11 +239,33 @@ case class TargetDisease(file: String) extends GqlCase[(String, String)] {
     val (target, disease) = inputs
     s"""
       "variables": {
-      "efoId": "$target",
-      "ensemblId": "$disease"
+      "efoId": "$disease",
+      "ensemblId": "$target"
     }
     """
   }
+}
+
+case class TargetAggregationfilter(file: String) extends GqlCase[(String, AggregationFilter)] {
+  val inputGenerator = for {
+    gene <- geneGenerator
+    aggregationFilter <- aggregationfilterGenerator
+  } yield (gene, aggregationFilter)
+
+  def generateVariables(inputs: (String, AggregationFilter)): String =
+    s"""
+      "variables": {
+      "ensemblId": "${inputs._1}",
+      "size": 10,
+      "index": 0,
+      "aggregationFilters": [
+        {
+        "name": "${inputs._2.name}",
+        "path": ${inputs._2.path.mkString("[\"", "\", \"", "\"]")}
+        }]
+    }
+    """
+
 }
 
 case class TargetDiseaseSize(file: String) extends GqlCase[(String, String, Int)] {
@@ -80,8 +277,8 @@ case class TargetDiseaseSize(file: String) extends GqlCase[(String, String, Int)
   def generateVariables(target: String, disease: String, size: Int): String = {
     s"""
       "variables": {
-      "efoId": "$target",
-      "ensemblId": "$disease",
+      "efoId": "$disease",
+      "ensemblId": "$target",
       "size": $size
     }
     """
@@ -97,8 +294,8 @@ case class TargetDiseaseSizeCursor(file: String) extends GqlCase[(String, String
   def generateVariables(target: String, disease: String, size: Int): String = {
     s"""
     "variables": {
-      "efoId": "$target",
-      "ensemblId": "$disease",
+      "efoId": "$disease",
+      "ensemblId": "$target",
       "size": $size,
       "cursor": null
     }
--- test/inputs/GqlItTestInputs.scala
+++ test/inputs/GqlItTestInputs.scala
@@ -1,5 +1,6 @@
 package inputs
 
+import models.entities.AggregationFilter
 import org.scalacheck.Gen
 
 import scala.reflect.io.File
@@ -8,17 +9,91 @@ trait GqlItTestInputs {
 
   lazy val geneInputs = File(this.getClass.getResource(s"/gqlInputs/genes.txt").getPath).lines.toList
   lazy val diseaseInputs = File(this.getClass.getResource(s"/gqlInputs/efos.txt").getPath).lines.toList
-  lazy val drugInputs = Seq()
+  lazy val drugInputs = Seq("CHEMBL1430")
+
+  val aggregationFilterMap: Map[String, Seq[String]] = Map(
+    "pathwayTypes" -> Seq(
+      "Autophagy",
+      "Cell Cycle",
+      "Cell-Cell communication",
+      "Cellular responses to external stimuli",
+      "Chromatin organization",
+      "Circadian Clock",
+      "Developmental Biology",
+      "Digestion and absorption",
+      "Disease",
+      "DNA Repair",
+      "DNA Replication",
+      "Extracellular matrix organization",
+      "Gene expression (Transcription)",
+      "Hemostasis",
+      "Immune System",
+      "Metabolism",
+      "Metabolism of proteins",
+      "Metabolism of RNA",
+      "Muscle contraction",
+      "Neuronal System",
+      "Organelle biogenesis and maintenance",
+      "Programmed Cell Death",
+      "Protein localization",
+      "Reproduction",
+      "Sensory Perception",
+      "Signal Transduction",
+      "Transport of small molecules",
+      "Vesicle-mediated transport"
+    ),
+    "targetClasses" -> Seq(
+      "Adhesion",
+      "Auxiliary transport protein",
+      "Enzyme",
+      "Epigenetic regulator",
+      "Ion channel",
+      "Membrane receptor",
+      "Other cytosolic protein",
+      "Other membrane protein",
+      "Other nuclear protein",
+      "Secreted protein",
+      "Surface antigen",
+      "Transcription factor",
+      "Transporter",
+      "Unclassified protein",
+      "Structural protein"
+    ),
+    "dataTypes" -> Seq("Genetic associations",
+      "Drugs",
+      "Text mining",
+      "RNA expression",
+      "Animal models",
+      "Somatic mutations"),
+    "tractabilityAntibody" -> Seq("Clinical precedence",
+      "Predicted tractable high confidence",
+      "Predicted tractable med low confidence"),
+    "tractabilitySmallMolecule" -> Seq("Clinical precedence",
+      "Discovery precedence",
+      "Predicted tractable")
+  )
 
   // Generators
+  val aggregationfilterGenerator: Gen[AggregationFilter] = {
+    for {
+      name <- Gen.oneOf(aggregationFilterMap.keySet)
+      paths <- Gen.someOf(aggregationFilterMap(name))
+    } yield AggregationFilter(name, paths)
+  }
+
   val geneGenerator: Gen[String] = Gen.oneOf(geneInputs)
   val diseaseGenerator: Gen[String] = Gen.oneOf(diseaseInputs)
+  val drugGenerator: Gen[String] = Gen.oneOf(drugInputs)
   val sizeGenerator: Gen[Int] = Gen.chooseNum(1, 10)
   val targetDiseaseSizeGenerator: Gen[(String, String, Int)] = for {
     gene <- geneGenerator
     disease <- diseaseGenerator
     size <- sizeGenerator
   } yield (gene, disease, size)
+  val targetDiseaseGenerator: Gen[(String, String)] = for {
+    gene <- geneGenerator
+    disease <- diseaseGenerator
+  } yield (gene, disease)
 
   val searchGenerator: Gen[String] = Gen.oneOf(geneGenerator, diseaseGenerator)
 }
