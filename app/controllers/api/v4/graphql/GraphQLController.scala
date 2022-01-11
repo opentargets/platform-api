e2219ba94aebc4d789b4632e5a5f86201642494b
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-01-11 21:37:21 +0000

    Finetune caching mechanism.
    Graphql typically return Http 200 even when there are errors (unless
    the query can't be parsed). We don't want to cache errors which might
    resolve themselves. We check for errors in the response body before
    passing it back.

--- app/controllers/api/v4/graphql/GraphQLController.scala
+++ app/controllers/api/v4/graphql/GraphQLController.scala
@@ -1,5 +1,6 @@
 package controllers.api.v4.graphql
 
+import akka.stream.{ActorMaterializer, Materializer}
 import controllers.api.v4.graphql.QueryMetadataHeaders.{GQL_OP_HEADER, GQL_VAR_HEADER}
 import models.entities.TooComplexQueryError
 import models.entities.TooComplexQueryError._
@@ -23,6 +24,7 @@ case class GqlQuery(query: String, variables: JsObject, operation: Option[String
 @Singleton
 class GraphQLController @Inject()(implicit
                                   ec: ExecutionContext,
+                                  mat: Materializer,
                                   dbTables: Backend,
                                   cache: AsyncCacheApi,
                                   cc: ControllerComponents,
@@ -31,6 +33,7 @@ class GraphQLController @Inject()(implicit
     with Logging {
 
   private val non200CacheDuration = Duration(10, "seconds")
+
   def options: Action[AnyContent] = Action {
     NoContent
   }
@@ -57,25 +60,52 @@ class GraphQLController @Inject()(implicit
     if (variables.trim == "" || variables.trim == "null") Json.obj()
     else Json.parse(variables).as[JsObject]
 
+  private def responseContainsErrors(response: Result): Future[Boolean] =
+    response.body.consumeData
+      .map(_.utf8String)
+      .map(Json.parse)
+      .map { json =>
+        (json \ "errors").isDefined
+      }
+
   private def cachedQuery(gqlQuery: GqlQuery): Future[Result] = {
-    val fromCache: Future[Option[Result]] = cache.get[Result](gqlQuery.toString)
-    val cacheResult: Future[Result] = fromCache.flatMap {
-      case Some(result) => Future.successful(result)
-      case None =>
-        logger.debug(s"Cache miss on $gqlQuery")
-        val queryResult = executeQuery(gqlQuery)
-        queryResult.andThen {
-          case Success(s) =>
-            if (s.header.status == HttpStatus.SC_OK) {
-              logger.info(s"Caching 200 response on $gqlQuery")
-              cache.set(gqlQuery.toString, s)
-            } else {
-              logger.debug(s"Temporarily caching non-200 response on $gqlQuery")
-              cache.set(gqlQuery.toString, s, non200CacheDuration)
-            }
-        }
+    def cacheable(op: Option[String]): Boolean = !op.contains("IntrospectionQuery")
+
+    if (cacheable(gqlQuery.operation)) {
+      val fromCache: Future[Option[Result]] = cache.get[Result](gqlQuery.toString)
+      val cacheResult: Future[Result] = fromCache.flatMap {
+        case Some(result) => Future.successful(result)
+        case None =>
+          logger.debug(s"Cache miss on ${gqlQuery.operation}: ${gqlQuery.variables}")
+          val queryResult = executeQuery(gqlQuery)
+          queryResult.andThen {
+            case Success(s) =>
+              if (s.header.status == HttpStatus.SC_OK) {
+                /*
+                All GraphQL responses which pass basic validation return status code 200. If something went wrong a field
+                returned called 'errors'. If there were no errors, this field isn't present.
+                 */
+                responseContainsErrors(s).onComplete {
+                  case Success(hasErrors) =>
+                    if (hasErrors) {
+                      logger.info(s"Temporarily caching 200 response with errors")
+                      cache.set(gqlQuery.toString, s, non200CacheDuration)
+                    } else {
+                      logger.info(
+                        s"Caching 200 response on ${gqlQuery.operation}: ${gqlQuery.query.filter(_ >= ' ')}"
+                      )
+                      cache.set(gqlQuery.toString, s)
+                    }
+                  case Failure(exception) => logger.error(exception.getMessage)
+                }
+              }
+          }
+      }
+      cacheResult
+    } else {
+      executeQuery(gqlQuery)
     }
-    cacheResult
+
   }
 
   private def executeQuery(
