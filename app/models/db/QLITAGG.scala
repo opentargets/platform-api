3f176dcef521f3498512fe559d64af349f4e7548
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-07-12 16:35:59 +0100

    Add documentation

--- app/models/db/QLITAGG.scala
+++ app/models/db/QLITAGG.scala
@@ -4,6 +4,31 @@ import esecuele.Column._
 import esecuele.{Functions => F, Query => Q, _}
 import play.api.Logging
 
+/** Helper class for querying the Literature tables in Clickhouse.
+ *
+ * There are two tables of relevance: literature and literature_index. Literature_index provides a
+ * lookup table so that we can go from a target, drug or disease back to the publications which
+ * mention those entities. Each of the rows has a score indicating how relevant the result is. Once
+ * we select a subset of the rows from literature_index we then look up the publication data
+ * (sentences) which provide more information about the exact match.
+ *
+ * This powers the `literatureOcurrences` (sic) section of the API.
+ *
+ * @param tableName
+ * "literature" in DB under default conditions.
+ * @param indexTableName
+ * "literature_index" in DB under default conditions.
+ * @param ids
+ * to find related literature for. These are either a target, drug or disease and can be found in
+ * the 'keywordId' field of the "literature_index" in the DB.
+ * @param size
+ * page size to return
+ * @param offset
+ * starting point for return set
+ * @param filterDate
+ * limit results to those between the specified dates. When no dates are specified, all records
+ * since 1900 are returned by default.
+ */
 case class QLITAGG(
     tableName: String,
     indexTableName: String,
@@ -28,21 +53,21 @@ case class QLITAGG(
   val T: Column = column(tableName) // "literature" in DB under default conditions
   val TIdx: Column = column(indexTableName) // "literature_index" in DB under default conditions
 
-  private def createDatePreWhere(dates: Option[(Int, Int, Int, Int)]): PreWhere = {
+  private def createDatePreWhere(dates: Option[(Int, Int, Int, Int)]): PreWhere =
     dates match {
       case Some(date) =>
         PreWhere(
           F.and(
             F.in(key, F.set(ids.map(literal).toSeq)),
             createDateFilter(date)
-          ))
+          )
+        )
       case None =>
         PreWhere(F.in(key, F.set(ids.map(literal).toSeq)))
     }
-  }
 
   // Find pmids related to Ids from literature_index
-  private def pmidsQ(select: Seq[Column], dates: Option[(Int, Int, Int, Int)] = None): Q = {
+  private def pmidsQ(select: Seq[Column], dates: Option[(Int, Int, Int, Int)] = None): Q =
     Q(
       Select(select),
       From(TIdx),
@@ -52,9 +77,8 @@ case class QLITAGG(
       OrderBy(F.sum(relevance.name).desc :: F.any(date.name).desc :: Nil),
       Limit(offset, size)
     )
-  }
 
-  private def createDateFilter(value: (Int, Int, Int, Int)): Column = {
+  private def createDateFilter(value: (Int, Int, Int, Int)): Column =
     F.and(
       F.greaterOrEquals(
         F.plus(F.multiply(year, literal(100)), month),
@@ -65,8 +89,6 @@ case class QLITAGG(
         literal((filterDate.get._3 * 100) + filterDate.get._4)
       )
     )
-  }
-
 
   /** Return the total number of publications with a selected date range, and the earliest
    * publication year as tuple (Long, Int). If no date range is selected, a default of 1900 is
