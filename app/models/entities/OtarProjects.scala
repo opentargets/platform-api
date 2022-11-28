11278b246cb8a7857b2c256d8cdb160c833c4a8a
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-11-28 21:10:36 +0000

    Update otar project to match ES schema.

--- app/models/entities/OtarProjects.scala
+++ app/models/entities/OtarProjects.scala
@@ -9,7 +9,7 @@ case class OtarProject(otarCode: String,
                        status: String,
                        projectName: String,
                        reference: String,
-                       integratesInPPP: Option[Boolean] = Some(false)
+                       integratesInPPP: Boolean
 )
 
 case class OtarProjects(efoId: String, rows: Seq[OtarProject])
@@ -18,12 +18,18 @@ object OtarProjects {
   implicit val config: JsonConfiguration.Aux[Json.MacroOptions] = JsonConfiguration(SnakeCase)
   implicit val otarProjectImpW: OWrites[OtarProject] = Json.writes[OtarProject]
 
-  implicit val otarProjectImpR: Reads[OtarProject] = Json.reads[OtarProject]
+  implicit val otarProjectImpR: Reads[OtarProject] =
+    ((JsPath \ "otar_code").read[String] and
+      (JsPath \ "status").read[String] and
+      (JsPath \ "project_name").read[String] and
+      (JsPath \ "reference").read[String] and
+      (JsPath \ "integrates_data_PPP").read[Boolean]
+      ) (OtarProject.apply _)
 
   implicit val otarProjectsImpW: OWrites[OtarProjects] = Json.writes[OtarProjects]
   implicit val otarProjectsImpR: Reads[OtarProjects] =
     (
       (__ \ "efo_id").read[String] and
         (__ \ "projects").readWithDefault[Seq[OtarProject]](Seq.empty)
-    )(OtarProjects.apply _)
+      ) (OtarProjects.apply _)
 }
