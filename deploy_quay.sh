1db90ff99c58de4292baf1debf0ed24b653b731b
James Hayhurst <hayhurst.jd@gmail.com>
2023-11-06 15:08:28 +0000

    handle difference in travis tag-branch

--- deploy_quay.sh
+++ deploy_quay.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "true" ] || [ "${TRAVIS_TAG}" != "" ]; then
-  docker pull "${QUAY_REPO}:${TRAVIS_BRANCH}" || true
-  docker build --pull --cache-from "${QUAY_REPO}:${TRAVIS_BRANCH}" --tag "${QUAY_REPO}" . || docker build .
+  if [ "${TRAVIS_TAG}" != "" ]; then
+      docker_tag_label="${TRAVIS_TAG}"
+    else
+      docker_tag_label="${TRAVIS_BRANCH}"
+    fi
+  docker pull "${QUAY_REPO}:${docker_tag_label}" || true
+  docker build --pull --cache-from "${QUAY_REPO}:${docker_tag_label}" --tag "${QUAY_REPO}" . || docker build .
   docker login -u="${QUAY_USER}" -p="${QUAY_PASSWORD}" quay.io
   git_sha="${TRAVIS_COMMIT}"
-  docker tag "${QUAY_REPO}" "${QUAY_REPO}:${TRAVIS_BRANCH}"
-  docker tag "${QUAY_REPO}" "${QUAY_REPO}:${TRAVIS_COMMIT}-${TRAVIS_BRANCH}"
+  docker tag "${QUAY_REPO}" "${QUAY_REPO}:${docker_tag_label}"
+  docker tag "${QUAY_REPO}" "${QUAY_REPO}:${TRAVIS_COMMIT}-${docker_tag_label}"
   docker push "${QUAY_REPO}:${TRAVIS_BRANCH}" && docker push "${QUAY_REPO}:${git_sha}-${TRAVIS_BRANCH}"
   if [ "${TRAVIS_BRANCH}" = "master" ]; then
     docker tag "${QUAY_REPO}:${TRAVIS_BRANCH}" "${QUAY_REPO}:latest"
