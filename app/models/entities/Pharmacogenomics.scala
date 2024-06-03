b09d097930c164fab25feb5de286da80f361bfe9
James Hayhurst <hayhurst.jd@gmail.com>
2024-06-03 13:54:59 +0000

    update pharmacogenomics model for multitple drugs

--- app/models/entities/Pharmacogenomics.scala
+++ app/models/entities/Pharmacogenomics.scala
@@ -3,12 +3,17 @@ package models.entities
 import play.api.Logging
 import play.api.libs.json._
 
+case class Drug(
+    drugId: Option[String],
+    drugFromSource: Option[String]   
+)
+
 case class Pharmacogenomics(
     datasourceId: Option[String],
     datatypeId: Option[String],
-    drugId: Option[String],
-    drugFromSource: Option[String],
+    drugs: Option[Seq[Drug]],
     evidenceLevel: Option[String],
+    genotype: Option[String],
     genotypeAnnotationText: Option[String],
     genotypeId: Option[String],
     haplotypeFromSourceId: Option[String],
@@ -25,6 +30,8 @@ case class Pharmacogenomics(
 )
 
 object Pharmacogenomics extends Logging {
+  implicit val drugW: OWrites[Drug] = Json.writes[Drug]
+  implicit val drugF: OFormat[Drug] = Json.format[Drug]
   implicit val pharmacogenomicsW: OWrites[Pharmacogenomics] = Json.writes[Pharmacogenomics]
   implicit val pharmacogenomicsF: OFormat[Pharmacogenomics] = Json.format[Pharmacogenomics]
 }
