95c47131b092ee9ab9c0bad76c56c435e4f81fe0
James Hayhurst <hayhurst.jd@gmail.com>
2024-07-03 15:09:15 +0000

    update tests

--- test/inputs/GqlItTestInputs.scala
+++ test/inputs/GqlItTestInputs.scala
@@ -1,6 +1,5 @@
 package inputs
 
-import models.entities.AggregationFilter
 import org.scalacheck.Gen
 
 import scala.reflect.io.File
@@ -16,82 +15,7 @@ trait GqlItTestInputs {
   lazy val goInputs =
     File(this.getClass.getResource(s"/gqlInputs/goIds.txt").getPath).lines().toList
 
-  val aggregationFilterMap: Map[String, Seq[String]] = Map(
-    "pathwayTypes" -> Seq(
-      "Autophagy",
-      "Cell Cycle",
-      "Cell-Cell communication",
-      "Cellular responses to external stimuli",
-      "Chromatin organization",
-      "Circadian Clock",
-      "Developmental Biology",
-      "Digestion and absorption",
-      "Disease",
-      "DNA Repair",
-      "DNA Replication",
-      "Extracellular matrix organization",
-      "Gene expression (Transcription)",
-      "Hemostasis",
-      "Immune System",
-      "Metabolism",
-      "Metabolism of proteins",
-      "Metabolism of RNA",
-      "Muscle contraction",
-      "Neuronal System",
-      "Organelle biogenesis and maintenance",
-      "Programmed Cell Death",
-      "Protein localization",
-      "Reproduction",
-      "Sensory Perception",
-      "Signal Transduction",
-      "Transport of small molecules",
-      "Vesicle-mediated transport"
-    ),
-    "targetClasses" -> Seq(
-      "Adhesion",
-      "Auxiliary transport protein",
-      "Enzyme",
-      "Epigenetic regulator",
-      "Ion channel",
-      "Membrane receptor",
-      "Other cytosolic protein",
-      "Other membrane protein",
-      "Other nuclear protein",
-      "Secreted protein",
-      "Surface antigen",
-      "Transcription factor",
-      "Transporter",
-      "Unclassified protein",
-      "Structural protein"
-    ),
-    "dataTypes" -> Seq(
-      "Genetic associations",
-      "Drugs",
-      "Text mining",
-      "RNA expression",
-      "Animal models",
-      "Somatic mutations"
-    ),
-    "tractabilityAntibody" -> Seq(
-      "Clinical precedence",
-      "Predicted tractable high confidence",
-      "Predicted tractable med low confidence"
-    ),
-    "tractabilitySmallMolecule" -> Seq(
-      "Clinical precedence",
-      "Discovery precedence",
-      "Predicted tractable"
-    )
-  )
-
   // Generators
-  val aggregationfilterGenerator: Gen[AggregationFilter] = {
-    for {
-      name <- Gen.oneOf(aggregationFilterMap.keySet)
-      paths <- Gen.someOf(aggregationFilterMap(name))
-    } yield AggregationFilter(name, paths.to(Seq))
-  }
-
   val geneGenerator: Gen[String] = Gen.oneOf(geneInputs)
   val diseaseGenerator: Gen[String] = Gen.oneOf(diseaseInputs)
   val drugGenerator: Gen[String] = Gen.oneOf(drugInputs)
