28704b9a82ab821904110b411ba7b2dda866a800
ricardo <remo87@gmail.com>
2025-04-29 17:06:34 +0100

    agregar middleware y metricas de api

--- app/services/ApplicationStart.scala
+++ app/services/ApplicationStart.scala
@@ -1,8 +1,9 @@
 package services
 
 import com.google.inject.Singleton
-import io.prometheus.metrics.core.metrics.Counter
+import io.prometheus.metrics.core.metrics.{Counter, Histogram}
 import io.prometheus.metrics.instrumentation.jvm.JvmMetrics
+import io.prometheus.metrics.model.snapshots.Unit
 
 import javax.inject.Inject
 
@@ -13,10 +14,32 @@ class ApplicationStart @Inject() (
 
   JvmMetrics.builder().register()
 
-  //TODO: Register prometheus metrics
-  val counter: Counter = Counter
+  // TODO: Register prometheus metrics
+  val RequestCounter: Counter = Counter
     .builder()
-    .name("requests_total")
-    .help("total number of requests")
-    .labelNames("service").register
+    .name("platform_api_http_requests_total")
+    .help("total number of http requests")
+    .labelNames("handler", "method")
+    .register
+
+  val QueryTime: Histogram = Histogram
+    .builder()
+    .name("platform_api_graphql_query_time")
+    .help("histogram measuring the time taken for query execution")
+    .unit(Unit.SECONDS)
+    .register();
+
+  val FieldUsageCount: Counter = Counter
+    .builder()
+    .name("platform_api_graphql_field_usage")
+    .help("count the usages of different fields")
+    .labelNames("field_name")
+    .register()
+
+  val FieldErrorCount: Counter = Counter
+    .builder()
+    .name("platform_api_graphql_field_error")
+    .help("count the number of times that processing has failed on a given field")
+    .labelNames("field_name")
+    .register()
 }
