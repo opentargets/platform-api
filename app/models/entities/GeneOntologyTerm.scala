998dd1549796e4e054efefd2581b405df6010b21
James Hayhurst <hayhurst.jd@gmail.com>
2026-02-09 11:25:07 +0000

    add go to ch

--- app/models/entities/GeneOntologyTerm.scala
+++ app/models/entities/GeneOntologyTerm.scala
@@ -1,6 +1,7 @@
 package models.entities
 
 import play.api.libs.json.{Json, OFormat}
+import slick.jdbc.GetResult
 
 /** @param id
   *   of gene ontology, eg: GO:0005515
@@ -10,7 +11,8 @@ import play.api.libs.json.{Json, OFormat}
 case class GeneOntologyTerm(id: String, name: String)
 
 object GeneOntologyTerm {
-
+  implicit val getGoTermResult: GetResult[GeneOntologyTerm] =
+    GetResult(r => Json.parse(r.<<[String]).as[GeneOntologyTerm])
   implicit val geneOntologyF: OFormat[GeneOntologyTerm] = Json.format[GeneOntologyTerm]
 
 }
