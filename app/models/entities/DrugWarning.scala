3e6c42d618958611f58affc800342f5ebad28ee1
James Hayhurst <hayhurst.jd@gmail.com>
2026-02-26 16:43:46 +0000

    camel casing

--- app/models/entities/DrugWarning.scala
+++ app/models/entities/DrugWarning.scala
@@ -30,25 +30,8 @@ object DrugWarning extends OTLogging {
   implicit val getDrugWarningsFromDB: GetResult[DrugWarnings] =
     GetResult(r => Json.parse(r.<<[String]).as[DrugWarnings])
   implicit val drugWarningsImpF: OFormat[DrugWarnings] = Json.format[models.entities.DrugWarnings]
-  implicit val drugWarningsReferenceImpR: Reads[models.entities.DrugWarningReference] = (
-    (JsPath \ "ref_id").read[String] and
-      (JsPath \ "ref_type").read[String] and
-      (JsPath \ "ref_url").read[String]
-  )(DrugWarningReference.apply)
-  implicit val drugWarningReferenceImpW: OWrites[DrugWarningReference] =
-    Json.writes[DrugWarningReference]
-  implicit val drugWarningImpW: OWrites[models.entities.DrugWarning] = Json.writes[DrugWarning]
-  implicit val drugWarningImpR: Reads[models.entities.DrugWarning] = (
-    (JsPath \ "toxicityClass").readNullable[String] and
-      (JsPath \ "chemblIds").readNullable[Seq[String]] and
-      (JsPath \ "country").readNullable[String] and
-      (JsPath \ "description").readNullable[String] and
-      (JsPath \ "id").readNullable[Long] and
-      (JsPath \ "references").readNullable[Seq[DrugWarningReference]] and
-      (JsPath \ "warningType").read[String] and
-      (JsPath \ "year").readNullable[Int] and
-      (JsPath \ "efo_term").readNullable[String] and
-      (JsPath \ "efo_id").readNullable[String] and
-      (JsPath \ "efo_id_for_warning_class").readNullable[String]
-  )(DrugWarning.apply)
+  implicit val drugWarningsReferenceImpF: OFormat[models.entities.DrugWarningReference] =
+    Json.format[models.entities.DrugWarningReference]
+  implicit val drugWarningImpF: OFormat[models.entities.DrugWarning] =
+    Json.format[models.entities.DrugWarning]
 }
