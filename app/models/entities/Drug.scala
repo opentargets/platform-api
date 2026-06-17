01b0306fdbe92d734901a4612119a97c3d6dca06
ricardo <remo87@gmail.com>
2026-06-17 15:49:48 +0100

    decouple drug label from target

--- app/models/entities/Drug.scala
+++ app/models/entities/Drug.scala
@@ -1,17 +1,18 @@
 package models.entities
 
-import models.entities.Target.labelAndSourceImpF
 import play.api.libs.json._
 import slick.jdbc.GetResult
 import utils.db.DbJsonParser.fromPositionedResult
 
 case class DrugReferences(source: String, ids: Seq[String])
 
+case class DrugLabelAndSource(label: String, source: String)
+
 case class Drug(
     id: String,
     name: String,
-    synonyms: Seq[LabelAndSource],
-    tradeNames: Seq[LabelAndSource],
+    synonyms: Seq[DrugLabelAndSource],
+    tradeNames: Seq[DrugLabelAndSource],
     childChemblIds: Option[Seq[String]], // Gone?
     drugType: String,
     crossReferences: Option[Seq[DrugReferences]],
@@ -24,6 +25,7 @@ case class Drug(
 object Drug {
   implicit val getResult: GetResult[Drug] = GetResult(fromPositionedResult[Drug])
   implicit val DrugXRefImpW: OFormat[DrugReferences] = Json.format[DrugReferences]
+  implicit val drugLabelAndSourceImpF: OFormat[DrugLabelAndSource] = Json.format[DrugLabelAndSource]
   implicit val drugImplicitR: Reads[Drug] = Json.reads[Drug]
   implicit val drugImplicitW: OWrites[Drug] = Json.writes[Drug]
 }
